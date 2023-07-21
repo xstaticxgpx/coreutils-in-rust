42947dccc8f92dd20dc528bdde1101e04deb6c89
xstaticxgpx <gabe@gabe.tech>
2023-07-21 02:26:54 -0400

    cat: poc

---
+++ examples/cat.rs
@@ -0,0 +1,42 @@
+/*
+ * https://github.com/coreutils/coreutils/blob/master/src/cat.c
+ *
+ * high-level breakdown:
+ *
+ * parse arguments
+ * determine output characteristics (fstat/etc)
+ * -> io_blksize?
+ * if not formatted set O_BINARY file mode for stdout
+ * start main loop, iterating over cli args, start by checking if input stdin ("-")
+ * -> if files are provided as args, iterate on those (stdin is completely ignored)
+ * --> if not formatted set O_BINARY file mode for stdin/file
+ * ---> if error then report, continue
+ * --> get fstat of the input descriptor
+ * ----> io_blksize? https://github.com/coreutils/coreutils/blob/master/src/ioblksize.h#L77
+ * --> FADVISE_SEQUENTIAL if regular file
+ * --> error if _non empty_ input file (regular) == output file, continue
+ * --> determine implementation (copy_cat / simple_cat / cat)
+ * ---> `copy_cat` if no output formatting options provided and input/output are regular files
+ * ---> `simple_cat` if no output formatting options but irregular files (stdin/stdout)?
+ * ---> `cat` if output is formatted
+ * --> chain results using bitwise AND operator (&=) on boolean (`ok`) to determine final exit code
+ * --> any failures results in non-zero exit code regardless of where it occurred in the loop
+ * -> continue loop if more files (arguments) remain
+ */
+
+
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
+    {
+        let lines = io::stdin().lines();
+        let mut stdout = io::stdout().lock();
+        for line in lines {
+            // How to determine missing new line here?
+            stdout.write(line.unwrap().as_bytes())?;
+            // This will add a newline making output mismatched
+            stdout.write(b"\n")?;
+        }
+    }
+    Ok(())
+}
