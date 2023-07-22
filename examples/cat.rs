10bf89ed5a48674b8fa3f32745ccd922ea74199e
xstaticxgpx <gabe@gabe.tech>
2023-07-22 06:03:30 -0400

    cat: first iteration of simple_cat

--- examples/cat.rs
+++ examples/cat.rs
@@ -17,6 +17,7 @@
  * --> error if _non empty_ input file (regular) == output file, continue
  * --> determine implementation (copy_cat / simple_cat / cat)
  * ---> `copy_cat` if no output formatting options provided and input/output are regular files
+ * ----> if error use `simple_cat`
  * ---> `simple_cat` if no output formatting options but irregular files (stdin/stdout)?
  * ---> `cat` if output is formatted
  * --> chain results using bitwise AND operator (&=) on boolean (`ok`) to determine final exit code
@@ -25,42 +26,43 @@
  */
 
 
-use std::io::{self, Read, Write, BufReader};
+use std::io::{self, Read, Write};
 
-fn main() -> io::Result<()> {
-    {
-        let stdin = io::stdin().lock();
-        let mut stdout = io::stdout().lock();
-        // TODO: determine why write calls are not fully buffered (see strace)
-        /*
-         * read(0, "\241\371\370\243m\34\0\2738\341%\363\3363o6T\17~\337\34Zt\276\325\311\327\31\351\232\2176"..., 131072) = 131072
-         * write(1, "\241\371\370\243m\34\0\2738\341%\363\3363o6T\17~\337\34Zt\276\325\311\327\31\351\232\2176"..., 131062) = 131062
-         * write(1, "\260p\r\347UM\253V\274\210", 10) = 10
-         * read(0, "\\\255_?\322\37\273)\312\24~'\240b\270&\202\352-\30Kr\215\247wq\233\335\333o\2\327"..., 131072) = 131072
-         * write(1, "\\\255_?\322\37\273)\312\24~'\240b\270&\202\352-\30Kr\215\247wq\233\335\333o\2\327"..., 130904) = 130904
-         * write(1, "<\324\371\0\355\211\272\221\271\34D\267H\16\312\326;\263\265\370\v+\307'\ts\254\264ql)\327"..., 168) = 168
-         */
-        // TODO: determine buffer size
-        // https://github.com/coreutils/coreutils/blob/6c343a55745a37a1d4d20f39a427170c37044a65/src/ioblksize.h
-        let io_bufsize = 1024*128; // in bytes
-        // Vec<u8> holds UTF-8 characters
-        let mut buffer: Vec<u8> = vec![];
-        let mut reader = BufReader::new(stdin);
-        loop {
-            // Use `take` and `read_to_end` to limit reads given the desired bufsize
-            // As compared to using `read_exact` which requires a sized vector
-            // which can ultimately leave trailing null bytes when reaching EOF
-            let mut handle = reader.by_ref().take(io_bufsize);
-            match handle.read_to_end(&mut buffer) {
-                Ok(0)  => { /* EOF */ break; }
-                // Using `.write()` here leads to missing data in output on large streams
-                Ok(..) => { stdout.write_all(&buffer)?; buffer.clear(); }
-                Err(_) => { break; }
-            };
-        }
-        // Does not seem needed?
-        stdout.flush()?;
-        buffer.clear();
+// TODO: determine buffer size
+const IO_BUFSIZE: u64 = 1024*128;
+
+fn simple_cat(mut stdin: std::io::StdinLock<>, mut stdout: std::io::StdoutLock<'_>) -> io::Result<()> {
+    // TODO: determine why write calls are not fully buffered with /dev/random (see strace)
+    // Using /dev/zero results in fully buffered writes (reads are always fully buffered?)
+    /*
+     * strace ./target/release/examples/cat <t/test.rand >t/test.rand2
+     * read(0, "\241\371\370\243m\34\0\2738\341%\363\3363o6T\17~\337\34Zt\276\325\311\327\31\351\232\2176"..., 131072) = 131072
+     * write(1, "\241\371\370\243m\34\0\2738\341%\363\3363o6T\17~\337\34Zt\276\325\311\327\31\351\232\2176"..., 131062) = 131062
+     * write(1, "\260p\r\347UM\253V\274\210", 10) = 10
+     * ###
+     * write(1, "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"..., 131072) = 131072
+     * read(0, "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"..., 131072) = 131072
+     */
+    let handle = stdin.by_ref();
+    let mut buffer: Vec<u8> = vec![]; // Vec<u8> holds UTF-8 characters
+    loop {
+        // Use `take` and `read_to_end` to limit reads given the desired bufsize
+        // As compared to using `read_exact` which requires a sized vector
+        // which can ultimately leave trailing null bytes when reaching EOF
+        match handle.take(IO_BUFSIZE).read_to_end(&mut buffer) {
+            Ok(0)  => { /* EOF */ break; }
+            // Using `.write_all()` here to ensure we aren't missing data in output on large streams
+            // Even doing `write()` and `flush()` here doesn't work and drops data randomly (?)
+            Ok(..) => { stdout.write_all(&buffer)?; buffer.clear(); }
+            Err(_) => { break; }
+        };
     }
     Ok(())
+}
+
+fn main() -> io::Result<()> {
+    let stdin = io::stdin().lock();
+    let stdout = io::stdout().lock();
+    // TODO: this doesn't work interactively when newline is submitted (needs Ctrl+D / EOF)
+    simple_cat(stdin, stdout)
 }
