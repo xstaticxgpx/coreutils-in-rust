24c0fff946e877ce0db27c9c871e94d7adaf926f
xstaticxgpx <gabe@gabe.tech>
2023-07-23 19:35:18 -0400

    cat: simple_cat implemented

--- examples/cat.rs
+++ examples/cat.rs
@@ -28,8 +28,8 @@
  */
 
 
-#[allow(unused_imports)]
 use std::fs::File;
+#[allow(unused_imports)]
 use std::io::{self, Read, Write, BufRead, BufWriter};
 use std::os::unix::io::FromRawFd;
 
@@ -37,33 +37,13 @@ use std::os::unix::io::FromRawFd;
 // TODO: get actual page size?
 const IO_BUFSIZE: u64 = 1 << 17; // or 2^17 or 131072 (bytes) or 32 page sizes (4Kb usually)
 #[allow(dead_code)]
+// TODO: for interactive cat use handle.read_until(NEWLINE_CH, &mut buffer)
 const NEWLINE_CH: u8  = 10; // 0x0A
 
 fn simple_cat(mut stdin: std::io::StdinLock<>, mut stdout: BufWriter<File>) -> io::Result<()> {
-    // TODO: determine why write calls are not fully buffered with /dev/random (see strace)
-    // Interesting... when piping input vs stdin file redirection the buffer sizes are different?
-    // Yes - linux kernel caps at 65536 byte (16 * 4Kb pages) max buffer across pipes by default (minimum 8192):
+    // Linux kernel caps at 65536 byte (16 * 4Kb pages) max buffer across pipes by default:
     // https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/include/linux/pipe_fs_i.h
     // PAGE_SIZE = 1 << 12; PIPE_DEF_BUFFERS = 16; PAGE_SIZE * PIPE_DEF_BUFFERS == 65536
-    // PAGE_SIZE = 1 << 12; PIPE_MIN_DEF_BUFFERS = 2; PAGE_SIZE * PIPE_MIN_DEF_BUFFERS == 8192
-    /*
-     * strace ./target/release/examples/cat <t/test.rand >t/test.rand2
-     * read(0, "\241\371\370\243m\34\0\2738\341%\363\3363o6T\17~\337\34Zt\276\325\311\327\31\351\232\2176"..., 131072) = 131072
-     * write(1, "\241\371\370\243m\34\0\2738\341%\363\3363o6T\17~\337\34Zt\276\325\311\327\31\351\232\2176"..., 131062) = 131062
-     * write(1, "\260p\r\347UM\253V\274\210", 10) = 10
-     * ###
-     * write(1, "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"..., 131072) = 131072
-     * read(0, "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"..., 131072) = 131072
-     *
-     * It's flushing writes on newline characters ??? (`strace -x -s $((2**17))`):
-     * write(1, ".....garbage.....\x0a", 40808) = 40808
-     * write(1, ".....garbage.........", , 152) = 152
-     *
-     * Oh my... StdoutLock wraps LineWriter:
-     * https://doc.rust-lang.org/std/io/struct.LineWriter.html
-     * https://github.com/rust-lang/rust/blob/8771282d4e7a5c4569e49d1f878fb3ba90a974d0/library/std/src/io/stdio.rs#L535
-     * https://github.com/rust-lang/rust/blob/8771282d4e7a5c4569e49d1f878fb3ba90a974d0/library/std/src/io/buffered/linewriter.rs
-     */
     let handle = stdin.by_ref();
     // HACK!
     // Padding the buffer here but immediately clear so we can avoid `mremap` during runtime
@@ -81,20 +61,24 @@ fn simple_cat(mut stdin: std::io::StdinLock<>, mut stdout: BufWriter<File>) -> i
             Ok(..) => { stdout.write_all(&buffer)?; buffer.clear(); }
             Err(_) => { break; }
         };
-        stdout.flush()?;
     }
+    stdout.flush()?;
     Ok(())
 }
 
 fn main() -> io::Result<()> {
+    /*
+     * Stdout/StdoutLock is wrapped by LineWriter which always flushes writes on newline char:
+     * https://doc.rust-lang.org/std/io/struct.LineWriter.html
+     * https://github.com/rust-lang/rust/blob/8771282d4e7a5c4569e49d1f878fb3ba90a974d0/library/std/src/io/stdio.rs#L535
+     * https://github.com/rust-lang/rust/blob/8771282d4e7a5c4569e49d1f878fb3ba90a974d0/library/std/src/io/buffered/linewriter.rs
+     *
+     * Instead, use BufWriter directly on the raw file desc (std::io::StdoutRaw is not exposed)
+     * This seems like a common complaint:
+     * https://github.com/rust-lang/rust/issues/58326
+     * https://github.com/rust-lang/libs-team/issues/148
+     */
     let stdin = io::stdin().lock();
-    // TODO: this doesn't work interactively when newline is submitted (needs Ctrl+D / EOF)
-    // This works - results in 8192 read buffer sizes and seemingly arbitrary writes (on newlines?)
-    //match handle.read_until(NEWLINE_CH, &mut buffer) {
-    unsafe { 
-        // Stdout is wrapped my LineWriter which flushes when newline characters, instead
-        // of performing fully buffered writes like expected. So, interface directly with the raw FD.
-        let stdout = BufWriter::new(File::from_raw_fd(1)); //BufWriter::new(io::stdout().lock());
-        simple_cat(stdin, stdout)
-    }
+    let stdout = BufWriter::new(unsafe { File::from_raw_fd(1) });
+    simple_cat(stdin, stdout)
 }
